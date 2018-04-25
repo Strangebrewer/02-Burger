db04a02a692ce3b40b447da5c66d70e77beba24f
Strangebrewer <BKAShambala@gmail.com>
2018-04-25 03:22:04 -0600

    initial file structure as given in the assignment

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+var connection = require("connection.js");
+
+module.exports = function () {
+
+  function selectAll() {
+
+  }
+
+  function insertOne() {
+
+  }
+
+  function updateOne() {
+
+  }
+
+}
+
+
---
+++ controllers/burger_controller.js
@@ -0,0 +1,14 @@
+var express = require("express");
+// var path = require("path");
+// var exphbs = require("express-handlebars"); // these are not called for in the instructions - if they're not needed, remove them.
+var burger = require("../models/burger.js");
+
+
+//  'app' in there is a guess based on the friend finder assignment - remove if it doesn't make sense...
+function burgerRouter(app) {
+
+}
+
+module.exports = {
+  burgerRouter
+}
